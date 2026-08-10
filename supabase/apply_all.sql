503ce08cab75a924f13da3f59028aabb383e63ae
brunofreitasbm-web <brunofreitasbm@gmail.com>
2026-08-10 16:20:58 -0300

    feat: populate official fiscal data for Parque Shopping units in apply_all.sql

--- supabase/apply_all.sql
+++ supabase/apply_all.sql
@@ -1157,4 +1157,28 @@ alter table fa_kiosk_payments add column if not exists origin text check (origin
 update fa_kiosk_payments set origin = 'LOCAL' where origin is null;
 create index if not exists idx_fa_kiosk_payments_origin on fa_kiosk_payments (origin);
 
+-- Preenchimento dos dados fiscais oficiais da empresa Faça Amigos
+-- para as unidades Playground (Parque Shopping) e Circuito (Parque Shopping)
+-- extraídos da documentação CNPJ/IE/Sintegra.
+update fa_kiosk_units
+set
+  cnpj = '66.318.630/0001-17',
+  razao_social = 'FAÇA AMIGOS BRINQUEDOTECA LTDA',
+  nome_fantasia = 'FAÇA AMIGOS',
+  inscricao_estadual = '75.105.192-6',
+  cnae_principal = '9321-2-00',
+  crt = 1, -- Simples Nacional
+  end_logradouro = 'Rod. Augusto Montenegro',
+  end_numero = '4300',
+  end_complemento = 'Parque Shopping, 1º Piso - PSB01003',
+  end_bairro = 'Parque Verde',
+  end_municipio_ibge = '1501402', -- Belém / PA
+  end_uf = 'PA',
+  end_cep = '66635-110',
+  fone = '(91) 8250-1215',
+  address = 'Rod. Augusto Montenegro, 4300 - Parque Shopping Belém, Piso PSB01003, Parque Verde, Belém/PA - CEP 66635-110',
+  phone = '(91) 8250-1215'
+where name ilike '%Parque Shopping%' or name ilike '%Circuito%' or name ilike '%Playground%';
+
+
 
